1170c6e68acba7fb9e84e4d1412922c4096db2b1
jrfnl <jrfnl@users.noreply.github.com>
2022-02-19 23:50:31 +0100

    Tests: new `SettingsAddPathsTest`
    ... containing perfunctory tests for the `Settings::addPaths()` method.

---
+++ tests/Unit/SettingsAddPathsTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace PHP_Parallel_Lint\PhpParallelLint\Tests\Unit;
+
+use PHP_Parallel_Lint\PhpParallelLint\Settings;
+use PHP_Parallel_Lint\PhpParallelLint\Tests\UnitTestCase;
+
+/**
+ * @covers \PHP_Parallel_Lint\PhpParallelLint\Settings::addPaths
+ */
+class SettingsAddPathsTest extends UnitTestCase
+{
+    /**
+     * Test adding additional paths to the paths already in settings.
+     *
+     * @dataProvider dataAddPaths
+     *
+     * @param array $original The "original" value for $paths as set from the command-line.
+     * @param array $extra    Extra paths to add.
+     * @param array $expected Expected $paths.
+     *
+     * @return void
+     */
+    public function testAddPaths($original, $extra, $expected)
+    {
+        $settings        = new Settings();
+        $settings->paths = $original;
+
+        $settings->addPaths($extra);
+
+        $this->assertSame($expected, $settings->paths);
+    }
+
+    /**
+     * Data provider.
+     *
+     * @return array
+     */
+    public function dataAddPaths()
+    {
+        return array(
+            'No paths passed on CLI, no extra paths' => array(
+                'original' => array(),
+                'extra'    => array(),
+                'expected' => array(),
+            ),
+            'No paths passed on CLI, extra paths' => array(
+                'original' => array(),
+                'extra'    => array('path/1', 'path/subdir/2'),
+                'expected' => array('path/1', 'path/subdir/2'),
+            ),
+            'Paths passed on CLI, no extra paths' => array(
+                'original' => array('path/1', 'path/subdir/2'),
+                'extra'    => array(),
+                'expected' => array('path/1', 'path/subdir/2'),
+            ),
+            'Paths passed on CLI, extra paths, partially duplicate' => array(
+                'original' => array('path/1', 'path/subdir/2'),
+                'extra'    => array('path/1', 'path/subdir/3'),
+                'expected' => array('path/1', 'path/subdir/2', 'path/1', 'path/subdir/3'),
+            ),
+        );
+    }
+}
