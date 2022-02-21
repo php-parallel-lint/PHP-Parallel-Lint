ab966640d464930661ca218136c4d34eb7376a73
jrfnl <jrfnl@users.noreply.github.com>
2022-02-22 00:23:41 +0100

    BUG: JSON based reports (gitlab/json) break completely on this file!!!!
    Probably to do with escaping/character encoding which needs to be fixed

---
+++ tests/fixtures/duplicate-function-declaration-non-utf8-names.php
@@ -0,0 +1,9 @@
+<?php
+
+// Note: this file is saved as non-UTF8 on purpose! Do not change the file encoding.
+
+// Cannot redeclare foo() (previously declared in ....file.php:5) in ...
+
+function déjàVu() {}
+
+function déjàVu() {}
