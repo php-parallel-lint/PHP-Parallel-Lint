10dfbe8c8a55564db950d28c60b22aca1cd4f272
jrfnl <jrfnl@users.noreply.github.com>
2024-03-16 19:18:19 +0100

    Align the percentage output
    Left pad with whitespace so that the 100% line doesn't pop out of alignment.

--- src/Output.php
+++ src/Output.php
@@ -348,9 +348,9 @@ class TextOutput implements Output
 
     protected function writePercent()
     {
-        $percent = floor($this->checkedFiles / $this->totalFileCount * 100);
+        $percent = $this->stringWidth(floor($this->checkedFiles / $this->totalFileCount * 100), 3);
         $current = $this->stringWidth($this->checkedFiles, strlen($this->totalFileCount));
-        $this->writeLine(" $current/$this->totalFileCount ($percent %)");
+        $this->writeLine(" $current/$this->totalFileCount ($percent%)");
     }
 
     /**
