2b9c257be01d232256eb7ee5d5dfff737931ff6b
Denis Žoljom <denis.zoljom@infobip.com>
2024-04-12 16:44:01 +0200

    Add tests for paths with spaces

--- tests/Unit/SettingsAddPathsTest.php
+++ tests/Unit/SettingsAddPathsTest.php
@@ -59,6 +59,17 @@ class SettingsAddPathsTest extends UnitTestCase
                 'extra'    => array('path/1', 'path/subdir/3'),
                 'expected' => array('path/1', 'path/subdir/2', 'path/1', 'path/subdir/3'),
             ),
+            'Paths passed on CLI, paths with spaces' => array(
+                'original' => array('path with/spaces between/1', 'path with/spaces between/subdir/2'),
+                'extra'    => array('path/1', 'path/subdir/3', 'path/with spaces'),
+                'expected' => array(
+                    'path with/spaces between/1',
+                    'path with/spaces between/subdir/2',
+                    'path/1',
+                    'path/subdir/3',
+                    'path/with spaces'
+                ),
+            )
         );
     }
 }
